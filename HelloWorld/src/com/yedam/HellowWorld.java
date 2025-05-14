9a0bec2b75574f3d9a2fc6039be7576dcef6473d
김준현 <kjh31160694@gmail.com>
2025-05-14 10:47:05 +0900

    첫번째 릴리즈

---
+++ HelloWorld/src/com/yedam/HellowWorld.java
@@ -0,0 +1,10 @@
+package com.yedam;
+// 자바 = 컴파일 과정을 거침 (***.java -> ***.class) class 파일은 기계어로 구성됨. 저장버튼 누르면 알아서 컴파일 시켜준다.
+public class HellowWorld {
+	// main 메소드
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("Hellow, World");
+		}	
+	}
+}
