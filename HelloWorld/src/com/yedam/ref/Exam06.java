ca6e00cc20ed32520014940ec133ac0489f8ca7f
김준현 <kjh31160694@gmail.com>
2025-05-16 17:53:05 +0900

    123

--- HelloWorld/src/com/yedam/ref/Exam06.java
+++ HelloWorld/src/com/yedam/ref/Exam06.java
@@ -1,8 +1,25 @@
-package com.yedam.ref;
+package sex02.verify.exam06;
 
-public class Exam06 {
-	public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Exam06{
+	public static void main(String[] args)  {
+		boolean run = true;
+		int studentNum = 0;
+		int[] scores = null;
+		Scanner snc = new Scanner(System.in);
 		
+		while(run) {
+			System.out.println("-----------------------------------------------------------------------------------");
+			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
+			System.out.println("-----------------------------------------------------------------------------------");
+			System.out.println("선택> ");
+			
+			int selectNo = Integer.parseInt(snc.nextLine());
+		}
 	}
-
+	
+	
 }
+	
+
